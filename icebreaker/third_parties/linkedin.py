82b7c81c49ddda26ba73c380cc2c81d8484f6512
jmbcmuphd <163065508+jmbcmuphd@users.noreply.github.com>
2024-08-03 17:00:17 -0400

    Started writing linkedin profile scraper func

---
+++ icebreaker/third_parties/linkedin.py
@@ -0,0 +1,35 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
+    """scrape information from LinkedIn profile page,
+    Manually scrape the information from LinkedIn profile"""
+
+    if mock:
+        linkedin_profile_url = f"{os.environ.get('MOCK_LINKEDIN_URL')}"
+        response = requests.get(linkedin_profile_url, timeout=10)
+    else:
+        api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+        header_dic = {"Authorization": f"Bearer {os.environ.get('PROXYCURL_API_KEY')}"}
+        response = requests.get(
+            api_endpoint,
+            params={"url": linkedin_profile_url},
+            headers=header_dic,
+            timeout=10,
+        )
+
+    data = response.json()
+
+    return data
+
+
+if __name__ == "__main__":
+    print(
+        scrape_linkedin_profile(
+            linkedin_profile_url="https://www.linkedin.com/in/eden-marco/", mock=True
+        )
+    )
