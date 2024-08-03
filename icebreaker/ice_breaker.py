472fda9fc5d0b254d1cc3dc79a9c19014f69dc33
jmbcmuphd <163065508+jmbcmuphd@users.noreply.github.com>
2024-08-03 19:24:26 -0400

    Created langchain agent to get LinkedIn URL and print out two useful facts about that person.

--- icebreaker/ice_breaker.py
+++ icebreaker/ice_breaker.py
@@ -5,31 +5,36 @@ from langchain.chains import LLMChain
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 import os
 
 
-if __name__ == "__main__":
-    load_dotenv()
-    print("Hello LangChain")
+def ice_break_with(name: str) -> str:
+    linkedin_username = linkedin_lookup_agent(name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
 
     summary_template = """
-        given the LinkedIn information {information} about a person from whom I want you to create:
-        1. a short summary
-        2. two interesting facts about them
-    """
+            given the LinkedIn information {information} about a person from whom I want you to create:
+            1. a short summary
+            2. two interesting facts about them
+        """
 
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
     )
 
-    llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
     # llm = ChatOllama(model="llama3.1")
-
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
     chain = summary_prompt_template | llm | StrOutputParser()
-    linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/eden-marco/", mock=True
-    )
+
     # chain = LLMChain(llm=llm, prompt=summary_prompt_template)
     res = chain.invoke(input={"information": linkedin_data})
 
     print(res)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    print("Ice Breaker Enter")
+
+    ice_break_with(name="Jon Krohn")
