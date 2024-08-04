8ab2bf5b205db3a3ed29d74fecf07c4eb62d8b3d
jmbcmuphd <163065508+jmbcmuphd@users.noreply.github.com>
2024-08-04 09:00:38 -0400

    Added pydantic output parser.

--- icebreaker/ice_breaker.py
+++ icebreaker/ice_breaker.py
@@ -6,26 +6,33 @@ from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parsers import summary_parser
 import os
 
 
 def ice_break_with(name: str) -> str:
     linkedin_username = linkedin_lookup_agent(name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
 
     summary_template = """
-            given the LinkedIn information {information} about a person from whom I want you to create:
+            given the information about a person from LinkedIn {information},
+            I want you to create:
             1. a short summary
             2. two interesting facts about them
+            
+            \n{format_instructions}
         """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={"format_instructions": summary_parser.get_format_instructions()},
     )
 
-    # llm = ChatOllama(model="llama3.1")
+    #  llm = ChatOllama(model="llama3.1")
     llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
-    chain = summary_prompt_template | llm | StrOutputParser()
+    #  chain = summary_prompt_template | llm | StrOutputParser()
+    chain = summary_prompt_template | llm | summary_parser
 
     # chain = LLMChain(llm=llm, prompt=summary_prompt_template)
     res = chain.invoke(input={"information": linkedin_data})
@@ -37,4 +44,4 @@ if __name__ == "__main__":
     load_dotenv()
     print("Ice Breaker Enter")
 
-    ice_break_with(name="Jon Krohn")
+    ice_break_with(name="Harrison Chase")
