c423c667de4b8fe7799edbb1f1404c7dab53fd47
jmbcmuphd <163065508+jmbcmuphd@users.noreply.github.com>
2024-08-06 19:08:05 -0400

    Added rag chain with custom rag prompt

--- intro-to-vector-dbs/main.py
+++ intro-to-vector-dbs/main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_pinecone import PineconeVectorStore
 from langchain import hub
@@ -8,6 +9,11 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
 
 load_dotenv()
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
 
 if __name__ == "__main__":
     print("Retrieving...")
@@ -34,4 +40,26 @@ if __name__ == "__main__":
 
     print(result)
 
+    template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, 
+    just say that you don't know, don't try to make up an answer. Use three sentences maximum and keep the answer 
+    as concise as possible. Always say "thanks for asking boss!" at the end of the answer.
+    
+    {context}
+    
+    Question: {question}
+    
+    Helpful Answer:"""
 
+    custom_rag_prompt = PromptTemplate.from_template(template=template)
+
+    rag_chain = (
+        {
+            "context": vectorstore.as_retriever() | format_docs,
+            "question": RunnablePassthrough(),
+        }
+        | custom_rag_prompt
+        | llm
+    )
+
+    res = rag_chain.invoke(query)
+    print(res)
