ae3755a34377112e4829cd5c63b25ab9182a7938
jmbcmuphd <163065508+jmbcmuphd@users.noreply.github.com>
2024-08-09 16:29:19 -0400

    Added alternate run_llm2 function.

--- documentation-helper/backend/core.py
+++ documentation-helper/backend/core.py
@@ -1,13 +1,33 @@
 import os
+from typing import Any
+
 from dotenv import load_dotenv
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain import hub
+from langchain.chains import RetrievalQA
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from pinecone import Pinecone
 from langchain_pinecone import PineconeVectorStore
+from langchain_community.vectorstores import Pinecone as PineconeLangChain
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 load_dotenv()
 index_name = os.environ["INDEX_NAME"]
+pinecone_api_key = os.environ["PINECONE_API_KEY"]
+pc = Pinecone(api_key=pinecone_api_key)
+
+
+def run_llm2(query: str) -> Any:
+    embeddings = OpenAIEmbeddings()
+    docsearch = PineconeLangChain.from_existing_index(index_name=index_name, embedding=embeddings)
+    chat = ChatOpenAI(model="gpt-4o-mini", verbose=True, temperature=0)
+    qa = RetrievalQA.from_chain_type(
+        llm=chat,
+        chain_type="stuff",
+        retriever=docsearch.as_retriever(),
+        return_source_documents=True,
+    )
+    return qa({"query": query})
 
 
 def run_llm(query: str):
@@ -25,6 +45,10 @@ def run_llm(query: str):
     result = qa.invoke(input={"input": query})
     return result
 
+
 if __name__ == "__main__":
     res = run_llm(query="What is a LangChain Chain?")
     print(res["answer"])
+
+    result = run_llm2(query="What is a LangChain chain?")
+    print(result["result"])
