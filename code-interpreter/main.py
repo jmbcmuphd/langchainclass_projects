1fbdf01bbb21293ef0c31d6495da9e12e6fd5a10
jmbcmuphd <163065508+jmbcmuphd@users.noreply.github.com>
2024-08-11 13:00:57 -0400

    Wrote simple CSV file agent

--- code-interpreter/main.py
+++ code-interpreter/main.py
@@ -1,8 +1,6 @@
 from dotenv import load_dotenv
-from langchain import hub
+from langchain_experimental.agents import create_csv_agent
 from langchain_openai import ChatOpenAI
-from langchain.agents import create_react_agent, AgentExecutor
-from langchain_experimental.tools import PythonAstREPLTool
 
 load_dotenv()
 
@@ -10,31 +8,18 @@ load_dotenv()
 def main():
     print("Start...")
 
-    instructions = """You are an agent designed to write and execute python code to answer questions.
-        You have access to a python REPL, which you can use to execute python code.
-        If you get an error, debug your code and try again.
-        Only use the output of your code to answer the question. 
-        You might know the answer without running any code, but you should still run the code to get the answer.
-        If it does not seem like you can write code to answer the question, just return "I don't know" as the answer.
-        """
-
-    base_prompt = hub.pull("langchain-ai/react-agent-template")
-    prompt = base_prompt.partial(instructions=instructions)
-
-    tools = [PythonAstREPLTool()]
-    agent = create_react_agent(
-        prompt=prompt, llm=ChatOpenAI(temperature=0, model="gpt-4o-mini"), tools=tools
+    csv_agent = create_csv_agent(
+        llm=ChatOpenAI(temperature=0, model="gpt-4"),
+        path="episode_info.csv",
+        verbose=True,
     )
 
-    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-    agent_executor.invoke(
-        input={
-            "input": """generate and save in current working directory 15 QRcodes that point to
-            www.udemy.com/cource/langchain, you have qrcode package installed already"""
-        }
+    csv_agent.invoke(
+        input={"input": "which season has the most episodes?"}
     )
-
-
+    csv_agent.invoke(
+        input={"input": "in the file episode_info.csv, which writer wrote the most episodes? How many did he write?"}
+    )
 
 
 if __name__ == "__main__":
